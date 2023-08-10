df0fb4059b4da4dfeea087f62fad6d1d8d99e9c4
songqiang <87302269+songqiang321@users.noreply.github.com>
2023-08-10 09:51:01 +0800

    Create tft_model.py

---
+++ libs/tft_model.py
@@ -0,0 +1,5 @@
+"""Temporal Fusion Transformer Model.
+
+Contains the full TFT architecture and associated components. Defines functions
+for training, evaluation and prediction using simple Pandas Dataframe inputs.
+"""
